c0f678a175f621b8c61a539b3969636e6e8c0ab1
suzukalight <mail@mkubara.com>
2019-09-04 14:22:36 +0900

    Fix transformer

--- src/client/.jest/transform.js
+++ src/client/.jest/transform.js
@@ -1,3 +1,8 @@
 module.exports = require('babel-jest').createTransformer({
-  rootMode: 'upward',
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: ['require-context-hook', '@babel/plugin-transform-modules-commonjs'],
 });
