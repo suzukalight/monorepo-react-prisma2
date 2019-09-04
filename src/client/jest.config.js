88b566fe8433dca7baf3d82965ef79439f30a86a
suzukalight <mail@mkubara.com>
2019-09-04 12:46:59 +0900

    Add jest configs for client

---
+++ src/client/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'client',
+  displayName: 'client',
+  verbose: true,
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/.jest/transform.js',
+  },
+  testMatch: ['<rootDir>/**/?(*.)(spec|test).(ts|js)?(x)'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.jest/__mocks__/file.js',
+    '\\.(styl|css|less|scss)$': '<rootDir>/.jest/__mocks__/style.js',
+  },
+  // setupFiles: ['<rootDir>/.jest/setup.js'],
+};
